dbaec336310606754f60917061b449f98ef1e6c6
James Inman <james@jamesinman.co.uk>
2024-08-28 20:56:01 +0100

    Update template.rb for Rails 7.2

--- template.rb
+++ template.rb
@@ -3,6 +3,10 @@ require "zip"
 
 gem "panda_cms"
 
+# Libaries that haven't updated yet for 7.2
+gem "awesome_nested_set", github: "AhmedAliIbrahim/awesome_nested_set", ref: "e0fdc7a792f703e0383976924c1d78202f0ba48a"
+gem "paper_trail", github: "kalashnikovisme/paper_trail", ref: "5343ce6b9b6ca0b850427c3e21aeed740c449939"
+
 # Choose a test framework
 test_framework = "minitest"
 
