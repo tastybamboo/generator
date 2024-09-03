59a1f337265eb3475ad929804a4cec982623ba84
James Inman <james@jamesinman.co.uk>
2024-09-03 23:48:18 +0100

    Don't force standardrb now rubocop-omakase in use

--- template.rb
+++ template.rb
@@ -3,10 +3,6 @@ require "zip"
 
 gem "panda_cms"
 
-# Libaries that haven't updated yet for 7.2
-gem "awesome_nested_set", github: "AhmedAliIbrahim/awesome_nested_set", ref: "e0fdc7a792f703e0383976924c1d78202f0ba48a"
-gem "paper_trail", github: "kalashnikovisme/paper_trail", ref: "5343ce6b9b6ca0b850427c3e21aeed740c449939"
-
 # Choose a test framework
 test_framework = "minitest"
 
@@ -14,15 +10,9 @@ if yes?("Do you want to replace minitest with rspec?")
   test_framework = "rspec"
   gem_group :development, :test do
     gem "rspec-rails"
-    gem "standard"
   end
 
   run "rm -rf test"
-else
-  # We're vaguely opinionated, so let's assume people want nice code... 🎋
-  gem_group :development, :test do
-    gem "standard"
-  end
 end
 
 # Download the theme and extract it into app/views
